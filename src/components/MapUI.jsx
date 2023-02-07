4546bd4b4206281fb21f2041f289848641c62176
AliHerzalla <93243926+AliHerzalla@users.noreply.github.com>
2023-02-07 12:48:43 +0300

    project Done

--- src/components/MapUI.jsx
+++ src/components/MapUI.jsx
@@ -6,25 +6,12 @@ import { getCenter } from "geolib";
 const MapUI = ({ data }) => {
   const [selectedLocation, setSelectedLocation] = useState({});
   const [viewport, setViewport] = useState({
+    width: "100%",
+    height: "100%",
     latitude: 37.7577,
     longitude: -122.4376,
     zoom: 11,
   });
-
-  const offsetTop = -10;
-  const offsetLeft = -20;
-  const offsetBottom = -45;
-
-  const markerOffsetObject = {
-    top: [0, offsetTop],
-    "top-left": [offsetLeft, offsetTop],
-    "top-right": [-offsetLeft, offsetTop],
-    bottom: [0, offsetBottom],
-    "bottom-left": [offsetLeft, offsetBottom],
-    "bottom-right": [-offsetLeft, offsetBottom],
-    left: [offsetLeft, offsetBottom],
-    right: [-offsetLeft, offsetBottom],
-  };
 
   const coordinates = data.map((result) => ({
     longitude: result.long,
@@ -40,7 +27,12 @@ const MapUI = ({ data }) => {
         latitude: center.latitude,
         zoom: viewport.zoom,
       }}
-      style={{ width: "100%", height: "100%", color: "black" }}
+      style={{
+        width: "100%",
+        maxWidth: "100%",
+        height: "100%",
+        color: "black",
+      }}
       mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
       mapStyle="mapbox://styles/ali-test-mapbox/cldt04011005701mg22xlici4"
     >
@@ -49,24 +41,24 @@ const MapUI = ({ data }) => {
           <Marker
             longitude={result.long}
             latitude={result.lat}
-            offsetLeft={-20}
-            offsetRight={-10}
           >
             <p
+              role="img"
+              aria-label="push-pin"
               onClick={() => setSelectedLocation(result)}
               className="cursor-pointer text-2xl animate-bounce"
             >
               📌
             </p>
           </Marker>
-          {selectedLocation.long == result.long ? (
+          {selectedLocation.long === result.long ? (
             <Popup
               onClose={() => setSelectedLocation({})}
               closeOnClick={true}
               latitude={result.lat}
               longitude={result.long}
             >
-              {res.title}
+              {result.title}
             </Popup>
           ) : null}
         </div>
